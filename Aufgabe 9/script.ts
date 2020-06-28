51045a241793d3ba7b7eca3ca9547661effd1a84
Michele-Chantal <64040092+Michele-Chantal@users.noreply.github.com>
2020-06-28 15:01:42 +0200

    Update Aufgabe 9

--- Aufgabe 9/script.ts
+++ Aufgabe 9/script.ts
@@ -1,4 +1,3 @@
-// Create a "close" button and append it to each list item
 var myToDolist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myToDolist.length; i++) {
@@ -9,7 +8,7 @@ for (i = 0; i < myToDolist.length; i++) {
   myToDolist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,7 +18,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
+
 var list = document.querySelector("ul");
 list.addEventListener("click", function(ev) {
   if (ev.target.tagName === "LI") {
@@ -27,7 +26,7 @@ list.addEventListener("click", function(ev) {
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
+
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("TxtInput").value;
